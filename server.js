de6b72f893b001cb35620c132983bfa57b9f01e4
Eduard Serban <serbanedi.fr@gmail.com>
2021-07-22 14:41:46 +0200

    adding cord

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const swiftRoutes = require('./routes/swift/routes-ios.js');
 const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
 const app = express();
 app.use(express.json());
 
+app.use(cors());
 app.use('/jello', jelloRoutes);
 app.use('/flutter', flutterRoutes);
 app.use('/ios', swiftRoutes);
