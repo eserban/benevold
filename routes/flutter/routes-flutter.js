3e09c45c2691ea58398933ddc8f5ee86e2c51906
Eduard Serban <serbanedi.fr@gmail.com>
2021-07-22 19:40:45 +0200

    flutter

--- routes/flutter/routes-flutter.js
+++ routes/flutter/routes-flutter.js
@@ -232,15 +232,15 @@ const client = new MongoClient(uri, {
 
         if (!token) {
             success = false;
-            code = 403;
+            code = 400;
             errorMessage = "Authentification Failed";
         }else if (!annId) {
             success = false;
-            code = 403;
+            code = 401;
             errorMessage = "Veuillez entrer l'id de l'annonce a effacer";
         } else if (annonce.length == 0) {
             success = false;
-            code = 403;
+            code = 402;
             errorMessage = "Cette annonce n'a pas été trouvée";
         } else {
             tokenObject = jwt.verify(token, process.env.JWT_KEY) ?? null;
