325294b9e7b271e46d5cc2ff2f5d0079de3707c1
Eduard Serban <serbanedi.fr@gmail.com>
2021-07-24 23:34:30 +0200

    main route

---
+++ routes/routes-main.js
@@ -0,0 +1,77 @@
+// définir
+var express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { usersFindSchema, jwtUserSignSchema, userSchema } = require("../modelsDB.js");
+var router = express.Router();
+let dbName = "benevold_db"
+
+const MongoClient = require("mongodb").MongoClient;
+const mongo = require("mongodb");
+const uri = process.env.MONGO_URI || "mongodb+srv://admin-benevold:MaqLBQjdNLmm6b4R@cluster0.qf07i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+(async() => {
+
+  await client.connect();
+
+
+  router.post('/user', async (req, res) => {
+    const token = req.header('access-token') ?? null;
+
+    const userId = req.body.user_id ?? null;
+    let userOid = new mongo.ObjectID(userId);
+
+    const type = req.query.type == "android" ? "old" : "teen";
+  
+    let tokenObject = null;
+    let userEmail = null;
+
+    let success         = true;
+    let code            = 200;
+    let errorMessage    = null;
+    let response        = [];
+
+    const userCollection    = await client.db(dbName).collection("users");
+    const user              = await userCollection.find({"_id": userOid, 'type': type}).limit(1).toArray();
+
+    if(!token){
+      success         = false;
+      code            = 403; 
+      errorMessage    = "Authentification Failed"
+    }else if(!userId){
+      success         = false;
+      code            = 400; 
+      errorMessage    = "Veuillez introduire les informationde l'utilisateur"
+    }else if(user.length == 0){
+      success         = false;
+      code            = 402; 
+      errorMessage    = "Cet utilisateur n'a pas été trouvé";
+    }else{
+      tokenObject = jwt.verify(token, process.env.JWT_KEY) ?? null;
+      if(!tokenObject){
+        success         = false;
+        code            = 500;
+        errorMessage    = "An error has occurred";
+      }
+    }
+
+    if (success) {
+      response = user[0];
+    }
+
+    const data = {
+      "success": success,
+      "requestCode": code,
+      "error": errorMessage,
+      "response" : response
+    };
+
+    res.status(code).send(data);
+      
+  });
+
+})();
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const jelloRoutes = require('./routes/jello/routes-jello.js');
 const flutterRoutes = require('./routes/flutter/routes-flutter.js');
 const swiftRoutes = require('./routes/swift/routes-ios.js');
 const authRoutes = require('./routes/auth/routes-auth.js');
+const mainRoutes = require('./routes/routes-main.js');
 const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 
@@ -14,6 +15,7 @@ app.use('/jello', jelloRoutes);
 app.use('/flutter', flutterRoutes);
 app.use('/ios', swiftRoutes);
 app.use('/auth', authRoutes);
+app.use('/', mainRoutes);
 
 
 // demander au serveur applicatif d'attendre des requêtes depuis le port spécifié plus haut
