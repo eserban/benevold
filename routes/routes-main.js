22c9e81cd623bc013c6a7d5706be355f2eeccf48
Eduard Serban <serbanedi.fr@gmail.com>
2021-07-25 00:29:23 +0200

    main route

--- routes/routes-main.js
+++ routes/routes-main.js
@@ -140,21 +140,8 @@ const client = new MongoClient(uri, {
 
   /**
    * //la route pour créer une annonce
-      POST /android/annonce :
-      Req {
-              user_id : user_id,
-              title: title, 
-              category: category,
-              description: description,
-              phone: phone,
-              email: email,
-              contact: contact, 
-              address: address,
-              date: date,
-              time: time
-      }
-    */
-  router.post('/annonce', async (req, res) => {
+  */
+  router.put('/annonce', async (req, res) => {
     const token = req.header('access-token') ?? null;
     
     const userId = req.body.user_id ?? null;
@@ -210,6 +197,78 @@ const client = new MongoClient(uri, {
     };
 
     res.status(code).send(data); 
+  });
+
+  /**
+   * //la route pour recuperer les infos d'annonce
+      POST /android/annonce :
+      Req {
+          annonce_id : annonce_id,
+      }
+      RES : {
+              user_id,
+              title: title, 
+              category: category,
+              description: description,
+              phone: phone,
+              email: email,
+              contact: contact, 
+              address: address,
+              date: date,
+              time: time
+      }
+   */
+
+  router.post('annonce', async (req, res) => {
+    const token = req.header('access-token') ?? null;
+
+    const annonceId = req.body.annonce_id ?? null;
+    let annonceOid = new mongo.ObjectID(annonceId);
+  
+    let tokenObject = null;
+    let userEmail = null;
+
+    let success         = true;
+    let code            = 200;
+    let errorMessage    = null;
+    let response        = [];
+
+    const annoncesCollection    = await client.db(dbName).collection("annonces");
+    const annonce              = await annoncesCollection.find({"_id": annonceOid}).toArray();
+
+    if(!token){
+      success         = false;
+      code            = 403; 
+      errorMessage    = "Authentification Failed"
+    }else if(!annonceId){
+      success         = false;
+      code            = 400; 
+      errorMessage    = "Veuillez introduire les informations de l'annonce"
+    }else if(annonce.length == 0){
+      success         = false;
+      code            = 402; 
+      errorMessage    = "Cette annonce n'a pas été trouvé";
+    }else{
+      tokenObject = jwt.verify(token, process.env.JWT_KEY) ?? null;
+      if(!tokenObject){
+        success         = false;
+        code            = 500;
+        errorMessage    = "An error has occurred";
+      }
+    }
+
+    if (success) {
+      response = annonce[0];
+    }
+
+    const data = {
+      "success": success,
+      "requestCode": code,
+      "error": errorMessage,
+      "response" : response
+    };
+
+    res.status(code).send(data);
   });
 
 
